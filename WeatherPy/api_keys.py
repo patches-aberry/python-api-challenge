497c5c66198a49bc2bf80083d9d94637c599ebf1
Patrick Aubry <patrickdaubry@gmail.com>
2023-04-03 18:29:02 -0600

    no message

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4d748f6df4796cefc6058df793da2090"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "602358ad23ee4649924b248adbc00982"
